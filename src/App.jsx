dd87b0f0f97b26f02a225d0aac02fbe4c3205a14
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-11-21 15:50:03 +0100

    first component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import Puzzle from "./components/Puzzle";
+import Bear from "./assets/Bear.jpg";
+
 
 function App() {
+
+
   return (
     <>
+      <Puzzle tittle="Bear" price="9,99$" img={Bear}></Puzzle>
+      <Puzzle></Puzzle>
+      <Puzzle></Puzzle>
       <Puzzle></Puzzle>
     </>
 
--- src/components/Puzzle.jsx
+++ src/components/Puzzle.jsx
@@ -1,12 +1,15 @@
-import puzzlePic from "../assets/Bear.jpg";
 
-function Puzzle() {
-return (<>
-<img src= {puzzlePic} alt="puzzle_img" />
-<h2>Puzzle</h2>
-<p>price</p>
+import "../components/Puzzle.css";
 
-</>)
+function Puzzle(props) {
+    return (<>
+        <div className="container">
+            <img src={props.img} alt={props.altText} className="picture" />
+            <h2 className="tittle">{props.tittle}</h2>
+            <h3 className="price">{props.price}</h3>
+            <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero laudantium repellendus placeat esse deleniti a quisquam voluptatem minima sit. Obcaecati necessitatibus ipsa quo! Dolorum eligendi dolorem repellat possimus neque quia.</p>
+        </div>
+    </>)
 
 }
 
