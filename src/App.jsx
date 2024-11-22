d3e5e1e81522557fad2061ab3012dd002c5ec791
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-11-22 09:51:52 +0100

    finished assignment1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Puzzle from "./components/Puzzle";
 import Logo from "./assets/logo.png";
 import Bear from "./assets/Bear.jpg";
-import "./main.css";
+import Cat from "./assets/Cat.jpg";
+import Birds from "./assets/Birds.jpg";
+import Corgi from "./assets/Corgi.jpg";
+import Fish from "./assets/Fish.jpg";
+import Dachsund from "./assets/Dachsund.jpg";
+import "./components/main.css";
 
 
 function App() {
@@ -20,11 +25,13 @@ function App() {
             </ul>
           </div>
         </nav>
-        <div>
+        <div className="item_container">
           <Puzzle tittle="Bear" price="9,99$" img={Bear} alt="puzzle bear picture"></Puzzle>
-          <Puzzle></Puzzle>
-          <Puzzle></Puzzle>
-          <Puzzle></Puzzle>
+          <Puzzle tittle="Cat" price="19,99$" img={Cat} alt="puzzle cat picture"></Puzzle>
+          <Puzzle tittle="Colibri birds" price="29,99$" img={Birds} alt="puzzle birds picture"></Puzzle>
+          <Puzzle tittle="Corgi dog" price="9,99$" img={Corgi} alt="puzzle corgi picture"></Puzzle>
+          <Puzzle tittle="Fish" price="9,99$" img={Fish} alt="puzzle fish picture"></Puzzle>
+          <Puzzle tittle="Dachsund dog" price="9,99$" img={Dachsund} alt="puzzle Dachsund picture"></Puzzle>
         </div>
 
       </div>
