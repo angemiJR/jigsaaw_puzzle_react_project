c4edeaf47d7d7c7a783320c4a18987baec23a203
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-11-22 09:10:10 +0100

    nav bar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Puzzle from "./components/Puzzle";
+import Logo from "./assets/logo.png";
 import Bear from "./assets/Bear.jpg";
+import "./main.css";
 
 
 function App() {
@@ -7,10 +9,25 @@ function App() {
 
   return (
     <>
-      <Puzzle tittle="Bear" price="9,99$" img={Bear}></Puzzle>
-      <Puzzle></Puzzle>
-      <Puzzle></Puzzle>
-      <Puzzle></Puzzle>
+      <div className="main">
+        <nav>
+          <img src={Logo} alt="Logo picture" />
+          <div>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
+        <div>
+          <Puzzle tittle="Bear" price="9,99$" img={Bear} alt="puzzle bear picture"></Puzzle>
+          <Puzzle></Puzzle>
+          <Puzzle></Puzzle>
+          <Puzzle></Puzzle>
+        </div>
+
+      </div>
     </>
 
   )
--- src/components/Puzzle.jsx
+++ src/components/Puzzle.jsx
@@ -5,7 +5,7 @@ function Puzzle(props) {
     return (<>
         <div className="container">
             <img src={props.img} alt={props.altText} className="picture" />
-            <h2 className="tittle">{props.tittle}</h2>
+            <h2 className="tittle">{props.tittle} jingsaw puzzle</h2>
             <h3 className="price">{props.price}</h3>
             <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero laudantium repellendus placeat esse deleniti a quisquam voluptatem minima sit. Obcaecati necessitatibus ipsa quo! Dolorum eligendi dolorem repellat possimus neque quia.</p>
         </div>
